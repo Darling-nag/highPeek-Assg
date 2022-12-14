b65e44a3ea9cc7625be44729c4b81e4a402f7e2f
Darling-nag <120501678+Darling-nag@users.noreply.github.com>
2022-12-14 16:59:45 +0530

    Add files via upload

---
+++ highPeek_Ass/github_proj/HighPeekAss.java
@@ -0,0 +1,80 @@
+package github_proj;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class HighPeekAss {
+	public static void main(String[] args) throws Exception {
+
+		FileInputStream fin = new FileInputStream("C:\\Users\\burus\\OneDrive\\Desktop\\input3.txt");
+		String s = "" + (char) fin.read();
+		int n = Integer.parseInt(s);
+		fin.read();
+		System.out.println(s);
+		ArrayList<String[]> ls = new ArrayList<String[]>();
+
+		int in = 0;
+		int c = 0;
+
+		int r = fin.read();
+		String str = "";
+		String[] ss = new String[3];
+		int i = 0;
+		while (r != -1) {
+
+			if (r == 13) {
+				System.out.println(str);
+				ss[i] = str;
+				str = "";
+				if (i == 2) {
+					ls.add(ss);
+					ss = new String[3];
+					i = 0;
+				} else
+					i++;
+
+			} else {
+				str = str + (char) r;
+				c++;
+			}
+
+			r = fin.read();
+			if (r == -1) {
+				System.out.println(str);
+				ss[i] = str;
+				ls.add(ss);
+			}
+		}
+		ss[i] = str;
+		System.out.println(c);
+
+		System.out.println(ls);
+		int max = 0;
+		for (String[] st : ls) {
+			int cp = Integer.parseInt(st[2].trim());
+			if (cp > max) {
+				max = cp;
+				in = ls.indexOf(st);
+			}
+
+		}
+		int sum = 0;
+		for (i = 0; i < ls.size(); i++) {
+			if (i != in)
+				sum = sum + Integer.parseInt(ls.get(i)[2].trim());
+
+		}
+		System.out.println(ls.size() - 1);
+		System.out.println(sum);
+
+		FileOutputStream fos = new FileOutputStream("C:\\Users\\burus\\OneDrive\\Desktop\\output3.txt");
+	  String r1=ls.size()-1+"";
+	  String s2=sum+"";
+		fos.write(r1.getBytes());
+		fos.write(13);
+		fos.write(s2.getBytes());
+		fos.close();
+		fin.close();
+	}
+}
